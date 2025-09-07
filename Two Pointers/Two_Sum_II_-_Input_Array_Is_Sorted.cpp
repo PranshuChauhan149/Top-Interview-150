9f41e807f7812a857e17906278e008d1101f9190
PranshuChauhan149 <pranshuchauhan149@gmail.com>
2025-09-07 23:49:25 +0530

    167. Two Sum II - Input Array Is Sorted

---
+++ Two Pointers/Two_Sum_II_-_Input_Array_Is_Sorted.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+  vector<int> twoSum(vector<int> &numbers, int target)
+  {
+
+    int n = numbers.size();
+    int i = 0;
+    int j = n - 1;
+    while (i < j)
+    {
+      int ele = numbers[i] + numbers[j];
+      if (ele > target)
+      {
+        j--;
+      }
+      else if (ele < target)
+      {
+        i++;
+      }
+      else
+      {
+        return {i + 1, j + 1};
+      }
+    }
+    return {};
+  }
+};
+int main()
+{
+}
