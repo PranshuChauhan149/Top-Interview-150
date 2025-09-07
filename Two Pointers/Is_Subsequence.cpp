bc870b75daae78e1f58eb01e17db7936cb54fbbe
PranshuChauhan149 <pranshuchauhan149@gmail.com>
2025-09-07 23:41:43 +0530

    392. Is Subsequence

---
+++ Two Pointers/Is_Subsequence.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        int i =0;
+        int j =0;
+        while(i<n and j<m){
+            if(s[i]==t[j]){
+                i++;
+                j++;
+            }
+           else{
+             j++;
+           }
+        }
+        if(i==n) return true;
+        return false;
+    }
+};
+int main()
+{
+}
